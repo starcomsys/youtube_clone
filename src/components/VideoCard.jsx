577cd14ee3f14a8c2b4e9ee28c44d10d947a11b2
Bobby <southazcoder@gmail.com>
2023-02-28 09:33:21 -0700

    Code sync 2

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,13 +11,15 @@ import {
 
 const VideoCard = ({ video: { id: {videoId}, snippet } }) => {
   return (
-    <Card sx={{ width: { md: '320px', xs: '100%'},
+    <Card sx={{ width: { xs: '100%', sm: '358px', md: '320px'},
         boxShadow: 'none', borderRadius: 0 }} >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl} >
         <CardMedia 
           image={snippet?.thumbnails?.high?.url} 
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: {
+            xs: '100%', sm: '358px'
+          }, height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1E1E1E', height: '106px' }}>
